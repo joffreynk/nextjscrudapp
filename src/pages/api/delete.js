b1a8c7e76d7cd6c87741d09fcc19527f16652570
JoffreyNK <joyjoffrey@gmail.com>
2023-05-23 15:40:22 +0300

    fix adding user when image has already uploaded

--- src/pages/api/delete.js
+++ src/pages/api/delete.js
@@ -4,7 +4,6 @@ import { connection } from './connection';
 export default function deleteuser(req, res) {
     try {
         const {userId, filepath} = req.body
-        console.log(req.body);
         if(fs.existsSync(filepath)) fs.unlinkSync(filepath)
         const sql = 'DELETE FROM users WHERE id = ?'
 
